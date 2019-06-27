e63a59c0069364560d32415115d442cdea03f760
LAPTOP-HG3TISJO\tp <249046859@qq.com>
2019-06-27 19:56:25 +0800

    增加管理员登录界面
    提交人：蒙坚

---
+++ src/components/AdminLogin.vue
@@ -0,0 +1,85 @@
+<template>
+  <body id="poster">
+  <el-form class="login-container" label-position="left"
+           label-width="0px">
+    <h3 class="login_title">管理员登录</h3>
+    <el-form-item>
+      <el-input type="text" v-model="loginForm.account"
+                auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-input type="password" v-model="loginForm.password"
+                auto-complete="off" placeholder="密码"></el-input>
+    </el-form-item>
+    <el-form-item style="width: 100%">
+      <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+    </el-form-item>
+  </el-form>
+  </body>
+</template>
+
+<script>
+
+  export default {
+    name: 'AdminLogin',
+    data () {
+      return {
+        loginForm: {
+          account: '116263000101',
+          password: '123456'
+        },
+        responseResult: []
+      }
+    },
+    methods: {
+      login () {
+        var _this = this
+        console.log(this.$store.state)
+        this.$axios
+          .post('/admin_login', {
+            account: this.loginForm.account,
+            password: this.loginForm.password
+          })
+          .then(successResponse => {
+            if (successResponse.data.code === 200) {
+              // var data = this.loginForm
+              _this.$store.commit('login', _this.loginForm)
+              var path = this.$route.query.redirect
+              this.$router.replace({path: path === '/' || path === undefined ? '/index' : path})
+            }
+          })
+          .catch(failResponse => {
+          })
+      }
+    }
+  }
+</script>
+
+<style>
+  #poster {
+    background: url("../assets/login_background.jpg") no-repeat center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 20px;
+  }
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
+import AdminLogin from '@/components/AdminLogin'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/admin_login',
+      name: 'AdminLogin',
+      component: AdminLogin
     },
     {
       path: '/index',
