6d962231819438adbf635ac41d33055b1cee6270
LAPTOP-HG3TISJO\tp <249046859@qq.com>
2019-06-27 16:42:35 +0800

    第二次提交 增加登录界面

--- src/main.js
+++ src/main.js
@@ -1,15 +1,43 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
 
+// 设置反向代理，前端请求默认发送到 http://localhost:8443/api
+var axios = require('axios')
+axios.defaults.baseURL = 'http://localhost:8443/api'
+// 全局注册，之后可在其他组件中通过 this.$axios 发送数据
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
+
+// 拦截后重定向
+router.beforeEach((to, from, next) => {
+    if (to.meta.requireAuth) {
+      if (store.state.user.username) {
+        next()
+      } else {
+        next({
+          path: 'login',
+          query: {redirect: to.fullPath}
+        })
+      }
+    } else {
+      next()
+    }
+  }
+)
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  render: h => h(App),
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AppIndex from '@/components/home/AppIndex'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/index',
+      name: 'AppIndex',
+      component: AppIndex,
+      meta: {
+        requireAuth: true
+      }
     }
   ]
 })
