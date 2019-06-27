748c42554d6dc4e2011be97cc8389e4dfc5ddde9
LAPTOP-HG3TISJO\tp <249046859@qq.com>
2019-06-27 16:46:28 +0800

    第三次提交 增加登录界面

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+// 用于拦截
+export default new Vuex.Store({
+  state: {
+    user: {
+      username: window.localStorage.getItem('user' || '[]') == null ? ''
+        : JSON.parse(window.localStorage.getItem('user' || '[]')).username
+    }
+  },
+  mutations: {
+    login (state, user) {
+      state.user = user
+      window.localStorage.setItem('user', JSON.stringify(user))
+    }
+  }
+})
